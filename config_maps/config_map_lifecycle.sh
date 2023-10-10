35ec7f23fa7c538c048259e23fee71e6e6e21511
Cat-Lord <matej.petras18@gmail.com>
2023-10-10 13:14:12 +0200

    Config map basics and different approaches

---
+++ config_maps/config_map_lifecycle.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# get parent directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/../cli_utils.sh"
+
+print_and_wait -c "Working with ConfigMaps"
+
+print_and_wait "A simple config map is a normal K8s resource"
+cat simple.configmap.yaml; echo
+
+print_and_wait "Created as any resource:"
+execute_command kubectl create -f simple.configmap.yaml
+
+print_and_wait "A more flexible approach is to create a config map using a configuration file"
+execute_command kubectl create configmap userconfig --from-env-file="user.conf"
+
+print_and_wait "We are also able to provide the key-value pairs directly via CLI"
+execute_command kubectl create configmap "userconfig-cli" --from-literal=appName=my-app --from-literal=myappversion=1.0.1
+
+print_and_wait "As with other resources, we can get a list of config maps"
+execute_command kubectl get configmaps
+
+print_and_wait "We can also see details about the config map"
+execute_command kubectl get configmap userconfig-cli -o=yaml
+
+print_and_wait "Let's now take a look at how we can utilize config maps in a different way when creating a pod."
+execute_command cat basic.pod.yaml
+execute_command kubectl apply -f basic.pod.yaml
+
+print_and_wait "Notice that we attach a config map as a volume. Let's see it in the pod"
+execute_command kubectl exec nginx-configmap --stdin --tty -- ls /etc/user-config/
+execute_command kubectl exec nginx-configmap --stdin --tty -- cat /etc/user-config/editor.height
+execute_command kubectl exec nginx-configmap --stdin --tty -- cat /etc/user-config/editor.width
+execute_command kubectl exec nginx-configmap --stdin --tty -- cat /etc/user-config/store-name
+
+print_and_wait "Now let's see that we have env variables available in our shell"
+# TODO: execute_command wont parse pipe correctly
+echo 'kubectl exec nginx-configmap --stdin --tty -- /bin/sh -c set | head -n 5'
+kubectl exec nginx-configmap --stdin --tty -- /bin/sh -c set | head -n 5
+echo
+
+print_and_wait "Cleaning up:"
+execute_command kubectl delete configmaps --all # there is a k8s config map, which will be recreated when deleted
+execute_command kubectl delete -f basic.pod.yaml
