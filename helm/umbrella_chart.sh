fed5ab2b8fca4c47eee372676bf5eefa19904b1e
Cat-Lord <matej.petras18@gmail.com>
2023-10-13 12:00:52 +0200

    Fixed mistakes in umbrella script

--- helm/umbrella_chart.sh
+++ helm/umbrella_chart.sh
@@ -8,11 +8,14 @@ print_and_wait -c "Installing application as umbrella chart."
 
 print_and_wait "Let's see how is the structure different for umbrella charts."
 execute_command tree app_umbrella
-execute_command cat app/app_umbrella/Chart.yaml
+execute_command cat app_umbrella/Chart.yaml
 
 print_and_wait "The process is the same as for a traditional chart."
 execute_command helm install catissimo app_umbrella
 
 print_and_wait "...with all the resources"
 execute_command kubectl get all
+
+print_and_wait "And cleaning up.."
+execute_command helm delete catissimo
 
