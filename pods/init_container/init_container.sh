bdce8f939e6cce49abc1fc5647bbbe44222e5b32
Cat-Lord <matej.petras18@gmail.com>
2023-10-31 13:39:24 +0100

    Basic InitContainer on Pod

---
+++ pods/init_container/init_container.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# get parent directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/../../cli_utils.sh"
+
+print_and_wait -c "Playing around with some init containers"
+
+print_and_wait "Firstly, let's see the pod yaml file. Notice the initContainers: we are waiting for a service 'cat-heaven' to be available"
+execute_command cat single.pod.yaml
+echo
+
+print_and_wait "Since we know that services are automatically added to the DNS, we can deploy a service later and it will satisfy the nslookup above. A simple service:"
+execute_command cat basic.service.yaml
+echo
+
+print_and_wait "Let's now deploy the pod"
+execute_command kubectl create -f single.pod.yaml
+
+print_and_wait "The information about the pod status looks interesting"
+execute_command kubectl get pods
+
+print_and_wait "We can access logs of the init container directly. The -c switch expects a container name to access."
+execute_command kubectl logs cat-servant -c wait-for-svc-container
+echo
+
+print_and_wait "Now let's see what happens when we deploy the service. We will deploy the service and immediatelly go into watch mode to see how the pod behaves. This may take ~10s byt you can continue with CTRL+C."
+execute_command kubectl create -f basic.service.yaml
+kubectl get pods -w
+echo
+
+print_and_wait "Seeing the logs should now reveal that the service was found and all is good."
+execute_command kubectl logs cat-servant -c wait-for-svc-container
+echo
+
+print_and_wait "Cleanup time:"
+execute_command kubectl delete -f single.pod.yaml
+execute_command kubectl delete -f basic.service.yaml
