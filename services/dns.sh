edfb2ab804e0a669330559f936423d6ed5355cfd
Cat-Lord <matej.petras18@gmail.com>
2023-11-06 13:46:47 +0100

    DNS networking sample. Updated service lifecycle script

---
+++ services/dns.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# get parent directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/../cli_utils.sh"
+
+print_and_wait -c "Services & the cluster DNS"
+
+execute_command kubectl create -f nginx.deployment.yaml
+
+execute_command kubectl expose deploy app-deployment --port 80 --target-port 8080
+
+print_and_wait "We've created a deployment, exposed it and now we will look at the DNS server:"
+execute_command kubectl get svc kube-dns --namespace kube-system
+DNS_SERVER_IP=`kubectl get svc kube-dns --namespace kube-system -o jsonpath='{ .spec.clusterIP }'`
+
+print_and_wait "With the DNS IP address we can use nslookup to query the available endpoints." 
+POD_NAME=`kubectl get pods -o jsonpath='{ .items[0].metadata.name }'`
+execute_command kubectl exec ${POD_NAME} -- nslookup app-deployment.default.svc.cluster.local ${DNS_SERVER_IP}
+
+print_and_wait "We can also inspect env variables:"
+echo "kubectl exec ${POD_NAME} -- env | sort"
+kubectl exec ${POD_NAME} -- env | sort
+
+print_and_wait "You might notice that we don't see the service above. This is because we first created the deployment and the service got created afterwards. Let's revert it and create the service first."
+
+execute_command kubectl delete -f nginx.deployment.yaml
+execute_command kubectl delete svc app-deployment
+execute_command kubectl create -f loadbalancer.service.yaml
+execute_command kubectl create -f nginx.deployment.yaml
+
+print_and_wait "Let's see now..."
+POD_NAME=`kubectl get pods -o jsonpath='{ .items[0].metadata.name }'`
+echo 'kubectl exec ${POD_NAME} -- env | sort'
+kubectl exec ${POD_NAME} -- env | sort
+print_and_wait "The service metadata get inserted into the pod as environment variables, yay!"
+echo
+
+print_and_wait "And finally let's create an externalName service."
+execute_command cat external.service.yaml
+execute_command kubectl create -f external.service.yaml
+
+print_and_wait "Now we can take a look at how the DNS resolves when queried from a pod."
+execute_command kubectl exec ${POD_NAME} -- nslookup external-svc.default.svc.cluster.local ${DNS_SERVER_IP}
+print_and_wait "We see that the service name got translated into the respective external name present in the config file."
+
+echo
+print_and_wait "In the end clean up the resources"
+execute_command kubectl delete all -l app=meowness
--- services/services_lifecycle.sh
+++ services/services_lifecycle.sh
@@ -14,21 +14,31 @@ print_and_wait "We can use port-forwarding to access the deployment. Navigate to
 execute_command kubectl port-forward deployment/app-deployment 8080:80
 
 print_and_wait "Did you notice a subtle detail in the command above? We are able to port-forward not only directly to pods, but also deployments (and services)!"
-print_and_wait "Let's get information about deployed pods and see how we can use services to access them."
+
+print_and_wait "We can expose services using a simple 'expose' command without an actual service yaml definition. We can define ports, type and other properties. Let's see it in action below."
+execute_command kubectl expose deployment app-deployment --port 80 --target-port 8080 --type ClusterIP
+execute_command kubectl get svc
+print_and_wait "Let's delete the service, clear the screen and continue."
+
+execute_command kubectl delete svc app-deployment
+print_and_wait "...press anything to continue"
+
+print_and_wait -c "Let's get information about deployed pods and see how we can use services to access them."
 
 echo 'kubectl get pods -o name --no-headers=true | head -n 1'
-POD_1_NAME=`kubectl get pods -o name --no-headers=true | head -n 1`
+POD_1_NAME=`kubectl get pods -o name --no-headers=true | head -n 1`	# e.g. pod/app-deployment-748f7fd48f-2sknn
 POD_1_IP=`kubectl get ${POD_1_NAME} --template '{{.status.podIP}}'`
 
 echo "Pod 1: ${POD_1_NAME} | ${POD_1_IP}"
 echo; # spacing
 
 print_and_wait "To access pods we can call an exposed service."
-print_and_wait "First we spin up a load-balancer service"
+print_and_wait "First we spin up a load-balancer service (wait until it's created and continue with CTRL+C)"
 execute_command cat loadbalancer.service.yaml
 execute_command kubectl apply -f loadbalancer.service.yaml
-sleep 2;
-execute_command kubectl get services
+
+print_and_wait "MAKE SURE the minikube tunnel is running, otherwise you might wait for just an eternity :)"
+execute_command kubectl get svc -w
 
 print_and_wait "Now let's use the name of the service instead of direct IP address"
 echo "kubectl get services --no-headers -o custom-columns=":metadata.name" | head -1"
@@ -36,10 +46,10 @@ kubectl get services --no-headers -o custom-columns=":metadata.name" | head -1; 
 SERVICE_DNS_NAME=`kubectl get services --no-headers -o custom-columns=":metadata.name" | head -1`
 execute_command kubectl exec ${POD_1_NAME} -- curl -s http://${SERVICE_DNS_NAME}:8080
 
-print_and_wait "Let's try a nodePort service"
+print_and_wait "Let's try a nodePort service (wait until it gets created and continue with CTRL+C)"
 execute_command cat nodeport.service.yaml
 execute_command kubectl apply -f nodeport.service.yaml
-sleep 2;
+execute_command kubectl get svc -w
 
 print_and_wait "We actually need to remove our load-balancer to be able to access pods via the nodePort service."
 execute_command kubectl delete service app-balancer
