daf5facdc1987c10fe2e4af36c3de94ead46fff1
Cat-Lord <matej.petras18@gmail.com>
2023-12-07 22:21:31 +0100

    Expanded pod example

--- pods/pod_lifecycle.sh
+++ pods/pod_lifecycle.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-
 print_and_wait -C "Pod basics using API & yaml"
+echo
 
-print_and_wait "List of all pods"
-execute_command kubectl get pods
+print_and_wait "There are pods that are deployed to provide Kubernetes-related services. These are normally not visible under the default namespace. We can get pods (and other resources) from all namespaces with a simple option:"
+execute_command kubectl get pods --all-namespaces
 
 print_and_wait "Now let's create a sample pod"
 execute_command kubectl run my-nginx --image=nginx:alpine
+
+print_and_wait "We can view existing pods like this:"
+execute_command kubectl get pods
 
 print_and_wait "Let's now see the details of the newly created pod"
 execute_command kubectl describe pod my-nginx
 
 print_and_wait "Now we forward a port to be able to access the pod from outside. You can check it by accessing localhost:8080 or continue by pressing CTRL+C to kill the port-forwarding process.."
-# print_and_wait "kubectl port-forward my-nginx 8080:80 &      # need to reference the job"
 execute_command "kubectl port-forward my-nginx 8080:80 > /dev/null &"
 execute_command curl http://localhost:8080
 print_and_wait "Let's close the port-forwarding process and continue"
@@ -33,6 +35,14 @@ execute_command kubectl create -f nginx.pod.yaml
 
 print_and_wait "Now we will delete the created pod using the same approach."
 execute_command kubectl delete -f nginx.pod.yaml
+
+print_and_wait "We can have multiple containers inside a single pod:"
+execute_command cat multi.pod.yaml
+execute_command kubectl create -f multi.pod.yaml
+print_and_wait "Wait for the pod to be ready and continue with CTRL+C"
+execute_command kubectl get pods -w
+print_and_wait "As we can see, even though we have deployed multiple containers, only one pod was created for them (based on our configuration)"
+execute_command kubectl delete -f multi.pod.yaml --now
 
 print_and_wait "We can check existing pods."
 execute_command kubectl get pods
